a181dfb5b3aac4d95676516b62365056f629a3e6
Chris Rycroft <chr@seas.harvard.edu>
2008-07-02 23:22:49 +0000

    More reorganization, plus Makefile support.

---
+++ examples/basic/import.cc
@@ -0,0 +1,35 @@
+// Voronoi calculation example code
+//
+// Author   : Chris H. Rycroft (LBL / UC Berkeley)
+// Email    : chr@alum.mit.edu
+// Date     : October 19th 2007
+
+#include "voro++.cc"
+
+// Set up constants for the container geometry
+const fpoint x_min=-5,x_max=5;
+const fpoint y_min=-5,y_max=5;
+const fpoint z_min=0,z_max=10;
+
+// Set up the number of blocks that the container is divided
+// into.
+const int n_x=6,n_y=6,n_z=6;
+
+int main() {
+	// Create a container with the geometry given above, and make it
+	// non-periodic in each of the three coordinates. Allocate space for
+	// eight particles within each computational block
+	container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
+			false,false,false,8);
+
+	//Randomly add particles into the container
+	con.import("pack_ten_cube");
+	
+	// Save the Voronoi network of all the particles to text files
+	// in gnuplot and POV-Ray formats
+	con.draw_cells_gnuplot("cells_ten_cube.gnu");
+	con.draw_cells_pov("cells_ten_cube.pov");
+
+	// Output the particles in POV-Ray format
+	con.draw_particles_pov("pack_ten_cube.pov");
+}
--- examples/basic/single_cell.cc
+++ examples/basic/single_cell.cc
@@ -2,9 +2,12 @@
 //
 // Author   : Chris H. Rycroft (LBL / UC Berkeley)
 // Email    : chr@alum.mit.edu
-// Date     : February 27th 2008
+// Date     : July 1st 2008
 
-#include "cell.cc"
+#include "../../src/voro++.cc"
+
+// This function returns a random floating point number between 0 and 1
+double rnd() {return double(rand())/RAND_MAX;}
 
 int main() {
 	double x,y,z,rsq,r;
@@ -17,9 +20,9 @@ int main() {
 	// Cut the cell by 250 random planes which are all a distance 1 away
 	// from the origin, to make an approximation to a sphere
 	for(int i=0;i<250;i++) {
-		x=double(2*rand()-1)/RAND_MAX;
-		y=double(2*rand()-1)/RAND_MAX;
-		z=double(2*rand()-1)/RAND_MAX;
+		x=2*rnd()-1;
+		y=2*rnd()-1;
+		z=2*rnd()-1;
 		rsq=x*x+y*y+z*z;
 		if(rsq>0.01&&rsq<1) {
 			r=1/sqrt(rsq);x*=r;y*=r;z*=r;
@@ -28,5 +31,5 @@ int main() {
 	}
 	
 	// Output the Voronoi cell to a file, in the gnuplot format
-	v.dump_gnuplot("test",0,0,0);
+	v.draw_gnuplot("single_cell.gnu",0,0,0);
 }
