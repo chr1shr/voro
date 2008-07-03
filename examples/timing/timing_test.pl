9c973c14dd7d315e6fc1d3f5f6f1e668a62a3b82
Chris Rycroft <chr@seas.harvard.edu>
2008-07-03 17:27:29 +0000

    Modified timing routine.

--- examples/timing/timing_test.pl
+++ examples/timing/timing_test.pl
@@ -2,7 +2,7 @@
 $tries=3;
 
 foreach $r (@range) {
-	system "g++ -I../../src -DNNN=$r -o timing_test -O3 -fast -pedantic -Winline --param large-function-growth=1000 --param max-inline-insns-single=2000 -Wall timing_test.cc";
+	system "g++ -I../../src -DNNN=$r -o timing_test -O3 -pedantic -Wall timing_test.cc";
 	$st=$stt=0;
 	foreach $t (1..$tries) {
 		system "./timing_test >time_temp";
@@ -15,3 +15,4 @@ foreach $r (@range) {
 	$stt=$stt/$tries-$st*$st;$stt=$stt>0?sqrt($stt):0;
 	print "$r $st $stt\n";
 }
+system "rm time_temp";
