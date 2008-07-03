d0e147f1f2c8f61aaee2a0926e4ef81242d91198
Chris Rycroft <chr@seas.harvard.edu>
2008-07-03 03:21:42 +0000

    Add voro++.cc and voro++.hh for easy access to the whole library.

---
+++ src/voro++.cc
@@ -0,0 +1,9 @@
+// Voro++, a 3D cell-based Voronoi library
+//
+// Author   : Chris H. Rycroft (LBL / UC Berkeley)
+// Email    : chr@alum.mit.edu
+// Date     : July 1st 2008
+
+#include "cell.cc"
+#include "container.cc"
+#include "wall.cc"
---
+++ src/voro++.hh
@@ -0,0 +1,14 @@
+// Voro++, a 3D cell-based Voronoi library
+//
+// Author   : Chris H. Rycroft (LBL / UC Berkeley)
+// Email    : chr@alum.mit.edu
+// Date     : July 1st 2008
+
+#ifndef VOROPP_HH
+#define VOROPP_HH
+
+#include "cell.hh"
+#include "container.hh"
+#include "wall.hh"
+
+#endif
