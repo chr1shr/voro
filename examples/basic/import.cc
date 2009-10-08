0d42d229a629a32d5e50ed972b809e3d8cc06967
Chris Rycroft <chr@seas.harvard.edu>
2009-10-08 23:03:09 +0000

    New periodic branch added. Some updates do dynamic branch.

--- examples/basic/import.cc
+++ examples/basic/import.cc
@@ -11,11 +11,11 @@ const fpoint x_min=-5,x_max=5;
 const fpoint y_min=-5,y_max=5;
 const fpoint z_min=0,z_max=10;
 
-// Set up the number of blocks that the container is divided
-// into.
+// Set up the number of blocks that the container is divided into
 const int n_x=6,n_y=6,n_z=6;
 
 int main() {
+
 	// Create a container with the geometry given above, and make it
 	// non-periodic in each of the three coordinates. Allocate space for
 	// eight particles within each computational block
@@ -27,10 +27,9 @@ int main() {
 
 	// Save the Voronoi network of all the particles to text files
 	// in gnuplot and POV-Ray formats
-//	con.draw_cells_gnuplot("cells_ten_cube.gnu");
-//	con.draw_cells_pov("cells_ten_cube.pov");
+	con.draw_cells_gnuplot("cells_ten_cube.gnu");
+	con.draw_cells_pov("cells_ten_cube.pov");
 
 	// Output the particles in POV-Ray format
-//	con.draw_particles_pov("pack_ten_cube.pov");
-	con.print_all_custom("%i %A %o");
+	con.draw_particles_pov("pack_ten_cube.pov");
 }
