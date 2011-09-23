7ff1cbe91d744c1db655cea2a82e9a300bc87ee9
Chris Rycroft <chr@seas.harvard.edu>
2011-09-23 06:56:31 +0000

    Added c_loop_order_periodic class.

--- src/c_loops.hh
+++ src/c_loops.hh
@@ -398,6 +398,65 @@ class c_loop_all_periodic : public c_loop_base {
 		}
 };
 
+/** \brief Class for looping over all of the particles specified in a
+ * pre-assembled particle_order class, for use with container_periodic classes.
+ *
+ * The particle_order class can be used to create a specific order of particles
+ * within the container. This class can then loop over these particles in this
+ * order. The class is particularly useful in cases where the ordering of the
+ * output must match the ordering of particles as they were inserted into the
+ * container. */
+class c_loop_order_periodic : public c_loop_base {
+	public:
+		/** A reference to the ordering class to use. */
+		particle_order &vo;
+		/** A pointer to the current position in the ordering class. */
+		int *cp;
+		/** A pointer to the end position in the ordering class. */
+		int *op;
+		/** The constructor copies several necessary constants from the
+		 * base class, and sets up a reference to the ordering class to
+		 * use.
+		 * \param[in] con the container class to use.
+		 * \param[in] vo_ the ordering class to use. */
+		template<class c_class>
+		c_loop_order_periodic(c_class &con,particle_order &vo_)
+		: c_loop_base(con), vo(vo_), nx(con.nx), oxy(con.nx*con.oy) {}
+		/** Sets the class to consider the first particle.
+		 * \return True if there is any particle to consider, false
+		 * otherwise. */
+		inline bool start() {
+			cp=vo.o;op=vo.op;
+			if(cp!=op) {
+				ijk=*(cp++);decode();
+				q=*(cp++);
+				return true;
+			} else return false;
+		}
+		/** Finds the next particle to test.
+		 * \return True if there is another particle, false if no more
+		 * particles are available. */
+		inline bool inc() {
+			if(cp==op) return false;
+			ijk=*(cp++);decode();
+			q=*(cp++);
+			return true;
+		}
+	private:
+		/** The number of computational blocks in the x direction. */
+		const int nx;
+		/** The number of computational blocks in a z-slice. */
+		const int oxy;
+		/** Takes the current block index and computes indices in the
+		 * x, y, and z directions. */
+		inline void decode() {
+			k=ijk/oxy;
+			int ijkt=ijk-oxy*k;
+			j=ijkt/nx;
+			i=ijkt-j*nx;
+		}
+};
+
 }
 
 #endif
