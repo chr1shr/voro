4615860b50cb11de9d5941f8b191976c0c7b6aa7
Chris Rycroft <chr@seas.harvard.edu>
2011-11-13 06:15:22 +0000

    Version number updates.

--- src/cmd_line.cc
+++ src/cmd_line.cc
@@ -23,7 +23,7 @@ const int max_regions=16777216;
 
 // This message gets displayed if the user requests the help flag
 void help_message() {
-	puts("Voro++ version 0.4.2, by Chris H. Rycroft (UC Berkeley/LBL)\n\n"
+	puts("Voro++ version 0.4.3, by Chris H. Rycroft (UC Berkeley/LBL)\n\n"
 	     "Syntax: voro++ [options] <x_min> <x_max> <y_min>\n"
 	     "               <y_max> <z_min> <z_max> <filename>\n\n"
 	     "By default, the utility reads in the input file of particle IDs and positions,\n"
@@ -110,7 +110,7 @@ void custom_output_message() {
 
 // Ths message is displayed if the user requests version information
 void version_message() {
-	puts("Voro++ version 0.4.2 (September 23rd 2011)");
+	puts("Voro++ version 0.4.3 (September 23rd 2011)");
 }
 
 // Prints an error message. This is called when the program is unable to make
