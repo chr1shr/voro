2e990bfb7a5dee6f74a631e956dd0f397dc1977a
Chris Rycroft <chr@seas.harvard.edu>
2011-12-13 20:40:54 +0000

    Updated container_periodic example.

--- examples/no_release/period.cc
+++ examples/no_release/period.cc
@@ -1,4 +1,3 @@
-
 #include "voro++.hh"
 using namespace voro;
 
@@ -7,36 +6,45 @@ const double bx=10;
 const double by=10;
 const double bz=10;
 const double bxy=0;
-const double bxz=0;
+const double bxz=5;
 const double byz=0;
 
 // Set up the number of blocks that the container is divided
 // into
 const int n_x=3,n_y=3,n_z=3;
 
+// Set the number of particles to add
+const int particles=20;
+
+// This function returns a random double between 0 and 1
+double rnd() {return double(rand())/RAND_MAX;}
+
 int main() {
+	int i;
+	double x,y,z;
 
 	// Create a container with the geometry given above, and make it
 	// non-periodic in each of the three coordinates. Allocate space for
 	// eight particles within each computational block.	
         container_periodic con(bx,bxy,by,bxz,byz,bz,n_x,n_y,n_z,8);
 
-	// Import the monodisperse test packing
-        con.import("pack_ten_cube");
+	// Add particles into the container at random positions
+	for(i=0;i<particles;i++) {
+		x=bx*rnd();
+		y=by*rnd();
+		z=bz*rnd();
+		con.put(i,x,y,z);
+	}
+
 
         // Output volume
         double vvol=con.sum_cell_volumes();
-        printf("Voronoi volume   : %g\n",vvol);
+        printf("Container volume : %g\n"
+	       "Voronoi volume   : %g\n",bx*by*bz,vvol);
 
-        // Output particle ID
-        con.print_custom("%i","pack_ten_cube_output.txt");
-
-        // Output the Voronoi tessellation in POV-Ray format.
-        con.draw_cells_pov("pack_ten_cube_v.pov");
-        con.draw_particles_pov("pack_ten_cube_p.pov");
-
-        // Output particle positions
-        con.draw_particles("pack_ten_cube_traj.txt");
-        con.draw_cells_gnuplot("pack_ten_cube_par.txt");
+        // Output particle positions, Voronoi cells, and the domain
+        con.draw_particles("particles_periodic.gnu");
+        con.draw_cells_gnuplot("cells_periodic.gnu");
+	con.draw_domain_gnuplot("domain_periodic.gnu");
 
 }
