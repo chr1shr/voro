b481b59e7c87689bc454bf62bc0f13e1160c98c4
Chris Rycroft <chr@seas.harvard.edu>
2016-11-02 17:24:17 +0000

    Quadtree computation working.

--- src/ctr_quad_2d.cc
+++ src/ctr_quad_2d.cc
@@ -2,18 +2,20 @@
 #include "quad_march.hh"
 
 #include <vector>
+#include <limits>
+#include <deque>
 
 namespace voro {
 
 container_quad_2d::container_quad_2d(double ax_,double bx_,double ay_,double by_) :
-	quadtree((ax_+bx_)*0.5,(ay_+by_)*0.5,(bx_-ax_)*0.5,(by_-ay_)*0.5),
-	ax(ax_), bx(bx_), ay(ay_), by(by_) {
+	quadtree((ax_+bx_)*0.5,(ay_+by_)*0.5,(bx_-ax_)*0.5,(by_-ay_)*0.5,*this),
+	ax(ax_), bx(bx_), ay(ay_), by(by_), bmask(0) {
 
 }
 
-quadtree::quadtree(double cx_,double cy_,double lx_,double ly_) :
-	cx(cx_), cy(cy_), lx(lx_), ly(ly_), ps(2), id(new int[qt_max]),
-	p(new double[ps*qt_max]), co(0), nco(0), nmax(0) {
+quadtree::quadtree(double cx_,double cy_,double lx_,double ly_,container_quad_2d &parent_) :
+	parent(parent_), cx(cx_), cy(cy_), lx(lx_), ly(ly_), ps(2),
+	id(new int[qt_max]), p(new double[ps*qt_max]), co(0), mask(0), nco(0), nmax(0) {
 
 }
 
@@ -30,10 +32,10 @@ quadtree::~quadtree() {
 
 void quadtree::split() {
 	double hx=0.5*lx,hy=0.5*ly;
-	qsw=new quadtree(cx-hx,cy-hy,hx,hy);
-	qse=new quadtree(cx+hx,cy-hy,hx,hy);
-	qnw=new quadtree(cx-hx,cy+hy,hx,hy);
-	qne=new quadtree(cx+hx,cy+hy,hx,hy);
+	qsw=new quadtree(cx-hx,cy-hy,hx,hy,parent);
+	qse=new quadtree(cx+hx,cy-hy,hx,hy,parent);
+	qnw=new quadtree(cx-hx,cy+hy,hx,hy,parent);
+	qne=new quadtree(cx+hx,cy+hy,hx,hy,parent);
 	for(int i=0;i<co;i++)
 		(p[ps*i]<cx?(p[ps*i+1]<cy?qsw:qnw)
 			   :(p[ps*i+1]<cy?qse:qne))->quick_put(id[i],p[ps*i],p[ps*i+1]);
@@ -63,6 +65,16 @@ void quadtree::draw_cross(FILE *fp) {
 	}
 }
 
+void quadtree::reset_mask() {
+	mask=0;
+	if(id==NULL) {
+		qsw->reset_mask();
+		qse->reset_mask();
+		qnw->reset_mask();
+		qne->reset_mask();
+	}
+}
+
 void container_quad_2d::draw_quadtree(FILE *fp) {
 	fprintf(fp,"%g %g\n%g %g\n%g %g\n%g %g\n%g %g\n",ax,ay,bx,ay,bx,by,ax,by,ax,ay);
 	draw_cross(fp);
@@ -87,6 +99,30 @@ void quadtree::draw_particles(FILE *fp) {
 		qne->draw_particles(fp);
 	} else for(int i=0;i<co;i++)
 		fprintf(fp,"%d %g %g\n",id[i],p[ps*i],p[ps*i+1]);
+}
+
+void quadtree::draw_cells_gnuplot(FILE *fp) {
+	if(id==NULL) {
+		qsw->draw_cells_gnuplot(fp);
+		qse->draw_cells_gnuplot(fp);
+		qnw->draw_cells_gnuplot(fp);
+		qne->draw_cells_gnuplot(fp);
+	} else {
+		voronoicell_2d c;
+		for(int j=0;j<co;j++) if(compute_cell(c,j))
+			c.draw_gnuplot(p[ps*j],p[ps*j+1],fp);
+	}
+}
+
+double quadtree::sum_cell_areas() {
+	if(id==NULL)
+		return qsw->sum_cell_areas()+qse->sum_cell_areas()
+		      +qnw->sum_cell_areas()+qne->sum_cell_areas();
+	double area=0;
+	voronoicell_2d c;
+	for(int j=0;j<co;j++) if(compute_cell(c,j))
+		area+=c.area();
+	return area;
 }
 
 void quadtree::setup_neighbors() {
@@ -146,6 +182,103 @@ void quadtree::add_neighbor_memory() {
 	for(int i=0;i<nco;i++) pp[i]=nei[i];
 	delete [] nei;
 	nei=pp;
+}
+
+bool quadtree::compute_cell(voronoicell_2d &c,int j) {
+	int i;
+	double x=p[ps*j],y=p[ps*j+1],x1,y1,xlo,xhi,ylo,yhi;
+	quadtree *q;
+
+	parent.initialize_voronoicell(c,x,p[ps*j+1]);
+
+	for(i=0;i<j;i++) {
+		x1=p[ps*i]-x;
+		y1=p[ps*i+1]-y;
+		if(!c.nplane(x1,y1,x1*x1+y1*y1,id[i])) return false;
+	}
+	i++;
+	while(i<co) {
+		x1=p[ps*i]-x;
+		y1=p[ps*i+1]-y;
+		if(!c.nplane(x1,y1,x1*x1+y1*y1,id[i])) return false;
+		i++;
+	}
+
+	unsigned int &bm=parent.bmask;
+	bm++;
+	if(bm==0) {
+		reset_mask();
+		bm=1;
+	}
+
+	mask=bm;
+	deque<quadtree*> dq;
+	for(i=0;i<nco;i++) {
+		dq.push_back(nei[i]);
+		nei[i]->mask=bm;
+	}
+
+	do {
+
+		q=dq.front();dq.pop_front();
+		q->bound(xlo,xhi,ylo,yhi);
+		xlo-=x;xhi-=x;
+		ylo-=y;yhi-=y;
+
+		if(xlo>0) {
+			if(ylo>0) {
+				if(corner_test(c,xlo,ylo,xhi,yhi)) continue;
+			} else if(yhi<0) {
+				if(corner_test(c,xlo,yhi,xhi,ylo)) continue;
+			} else {
+				if(edge_x_test(c,xlo,ylo,yhi)) continue;
+			}
+		} else if(xhi<0) {
+			if(ylo>0) {
+				if(corner_test(c,xhi,ylo,xlo,yhi)) continue;
+			} else if(yhi<0) {
+				if(corner_test(c,xhi,yhi,xlo,ylo)) continue;
+			} else {
+				if(edge_x_test(c,xhi,ylo,yhi)) continue;
+			}
+		} else {
+			if(ylo>0) {
+				if(edge_y_test(c,xlo,ylo,xhi)) continue;
+			} else if(yhi<0) {
+				if(edge_y_test(c,xlo,yhi,xhi)) continue;
+			} else voro_fatal_error("Compute cell routine revisiting central block, which should never\nhappen.",VOROPP_INTERNAL_ERROR);
+		}
+
+		for(i=0;i<q->co;i++) {
+			x1=q->p[ps*i]-x;
+			y1=q->p[ps*i+1]-y;
+			if(!c.nplane(x1,y1,x1*x1+y1*y1,id[i])) return false;
+		}
+
+		for(i=0;i<q->nco;i++) if(q->nei[i]->mask!=bm) {
+			dq.push_back(q->nei[i]);
+			q->nei[i]->mask=bm;
+		}
+	} while(!dq.empty());
+	return true;
+}
+
+inline bool quadtree::corner_test(voronoicell_2d &c,double xl,double yl,double xh,double yh) {
+	if(c.plane_intersects_guess(xl,yh,xl*xl+yl*yh)) return false;
+	if(c.plane_intersects(xh,yl,xl*xh+yl*yl)) return false;
+	return true;
+}
+
+inline bool quadtree::edge_x_test(voronoicell_2d &c,double xl,double y0,double y1) {
+	if(c.plane_intersects_guess(xl,y0,xl*xl)) return false;
+	if(c.plane_intersects(xl,y1,xl*xl)) return false;
+	return true;
+}
+
+inline bool quadtree::edge_y_test(voronoicell_2d &c,double x0,double yl,double x1) {
+	if(c.plane_intersects_guess(x0,yl,yl*yl)) return false;
+	if(c.plane_intersects(x1,yl,yl*yl)) return false;
+	return true;
 }
 
 }
