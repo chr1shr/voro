53b7ad2a04b580b0a7f68a68814c32fbbbacebc0
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-26 15:18:08 +0200

    generalize config.mk
    Reuse environment variables, if they are already set by the system by
    switching to += / ?= operator.
    See https://www.gnu.org/software/make/manual/html_node/Setting.html

--- config.mk
+++ config.mk
@@ -8,17 +8,17 @@
 # the Makefiles.
 
 # C++ compiler
-CXX=g++
+CXX?=g++
 
 # Flags for the C++ compiler
-CFLAGS=-Wall -ansi -pedantic -O3
+CFLAGS+=-Wall -ansi -pedantic -O3
 
 # Relative include and library paths for compilation of the examples
 E_INC=-I../../src
 E_LIB=-L../../src
 
 # Installation directory
-PREFIX=/usr/local
+PREFIX?=/usr/local
 
 # Install command
 INSTALL=install
