7ad6b0668d3a3179579acbd9f262f80e985131b7
Chris Rycroft <chr@seas.harvard.edu>
2022-06-12 22:26:01 -0400

    Update timing examples.

--- examples/timing/timing_test.cc
+++ examples/timing/timing_test.cc
@@ -1,60 +1,96 @@
-// Timing test example code
-//
-// Author   : Chris H. Rycroft (Harvard University / LBL)
-// Email    : chr@alum.mit.edu
-// Date     : August 30th 2011
+// Voro++, a cell-based Voronoi library
+// By Chris H. Rycroft and the Rycroft Group
 
-#include <ctime>
-using namespace std;
+#include <cstdlib>
+#include <cmath>
 
 #include "voro++.cc"
 using namespace voro;
 
-// Set up constants for the container geometry
-const double x_min=-1,x_max=1;
-const double y_min=-1,y_max=1;
-const double z_min=-1,z_max=1;
-
-// Set up the number of blocks that the container is divided into. If the
-// preprocessor variable NNN hasn't been passed to the code, then initialize it
-// to a good value. Otherwise, use the value that has been passed.
-#ifndef NNN
-#define NNN 26
+// Set up timing routine. If code was compiled with OpenMP, then use the
+// accurate wtime function. Otherwise use the clock function in the ctime
+// library.
+#ifdef _OPENMP
+#include "omp.h"
+inline double wtime_() {return omp_get_wtime();}
+#else
+inline double wtime_() {return 1./CLOCKS_PER_SEC*static_cast<double>(clock());}
 #endif
-const int n_x=NNN,n_y=NNN,n_z=NNN;
 
-// Set the number of particles that are going to be randomly introduced
-const int particles=100000;
+// Returns a random double that is uniformly distributed between 0 and 1
+inline double rnd() {return 1./RAND_MAX*static_cast<double>(rand());}
 
-// This function returns a random double between 0 and 1
-double rnd() {return double(rand())/RAND_MAX;}
+// Prints out a message about the syntax of the command-line utility
+void syntax_message() {
+    puts("Syntax: ./timing_test <num> <block_lo> <block_hi> <reps>\n"
+         "Arguments:\n"
+         "<num>         The number of particles       [100000]\n"
+         "<block_lo>    The lower limit of blocks     [10]\n"
+         "<block_hi>    The upper limit of blocks     [40]\n"
+         "<reps>        The number of repeat trails   [1]\n"
+         "<prd>         Whether to use periodicity    [0]\n\n"
+         "If any argument is missing, the default value in the square brackets is used\n");
+    exit(1);
+}
 
-int main() {
-	clock_t start,end;
-	int i;double x,y,z;
+int main(int argc,char **argv) {
 
-	// Create a container with the geometry given above, and make it
-	// periodic in each of the three coordinates. Allocate space for eight
-	// particles within each computational block.
-	container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
-			true,true,true,8);
+    // Check for a valid number of command-line arguments
+    if(argc>6) syntax_message();
 
-	//Randomly add particles into the container
-	for(i=0;i<particles;i++) {
-		x=x_min+rnd()*(x_max-x_min);
-		y=y_min+rnd()*(y_max-y_min);
-		z=z_min+rnd()*(z_max-z_min);
-		con.put(i,x,y,z);
-	}
+    // Read the command-line arguments, and check that they are valid
+    int blo=10,bhi=40,num=100000,reps=1;
+    bool prd=false;
+    if(argc>1) {
+        num=atoi(argv[1]);
+        if(num<=0) syntax_message();
+        if(argc>2) {
+            blo=atoi(argv[2]);
+            if(blo<=0) syntax_message();
+            if(argc>3) {
+                bhi=atoi(argv[3]);
+                if(bhi<blo) syntax_message();
+                if(argc>4) {
+                    reps=atoi(argv[4]);
+                    if(reps<=0) syntax_message();
+                    if(argc>5) {
+                        int k=atoi(argv[5]);
+                        if(k!=0&&k!=1) syntax_message();
+                        prd=k==1;
+                    }
+                }
+            }
+        }
+    }
 
-	// Store the initial clock time
-	start=clock();
+    // Loop over the block sizes to test
+    for(int b=blo;b<=bhi;b++) {
+        container_3d con(0,1,0,1,0,1,b,b,b,prd,prd,prd,8);
 
-	// Carry out a dummy computation of all cells in the entire container
-	con.compute_all_cells();
+        // Perform the repeat trials
+        double st=0,stt=0,sc=0,scc=0,t0,t1;
+        for(int l=0;l<reps;l++) {
 
-	// Calculate the elapsed time and print it
-	end=clock();
-	double runtime=double(end-start)/CLOCKS_PER_SEC;
-	printf("%g\n",runtime);
+            // Randomly insert the particles into the container
+            if(l!=0) con.clear();
+            t0=wtime_();
+            for(int i=0;i<num;i++) con.put(i,rnd(),rnd(),rnd());
+
+            // Perform the dummy computation to compute all the Voronoi cells
+            t1=wtime_();
+            con.compute_all_cells();
+
+            // Store the timing statistics
+            t0=t1-t0;t1=wtime_()-t1;
+            printf("%g %g\n",t0,t1);
+            st+=t0;stt+=t0*t0;
+            sc+=t1;scc+=t1*t1;
+        }
+
+        // Output the timing information
+        st/=reps;stt=stt/reps-st*st;
+        sc/=reps;scc=scc/reps-sc*sc;
+        printf("%d %g %g %g %g\n",b,st,stt<0?0:sqrt(stt),sc,scc<0?0:sqrt(scc));
+        con.clear();
+    }
 }
